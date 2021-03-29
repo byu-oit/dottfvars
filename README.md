5c40ef6756ee309cf2453efcb12991a5912bd403
Spencer Tuft <stuft2@byu.edu>
2021-03-29 16:24:13 -0600

    Improve documentation

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # dottfvars
+
+Parses a `.tfvars` or `.tfvars.json` file and adds the key value pairs
+to the node environment variables (`process.env`). This module is
+intended to be used in conjunction with the
+[`env-var` module](https://www.npmjs.com/package/env-var).
+
+## Usage
+
+Can parse `.tfvars` files
+
+```ts
+import {resolve} from 'path'
+
+dottfvars.from(resolve(__dirname, 'iac/development.tfvars'))
+const imageId = env.from(process.env).get('image_id').asString()
+```
+
+Alternatively, you may define your tfvars as JSON and pass in a
+`.tfvars.json` file path instead.
+
+Related Packages:
+- [env-ssm](https://github.com/byu-oit/env-ssm#readme)
--- test/dottfvars.spec.ts
+++ test/dottfvars.spec.ts
@@ -3,7 +3,7 @@ import * as env from 'env-var'
 import * as dottfvars from '../src/dottfvars'
 
 test('parses .tfvars file', async () => {
-  await dottfvars.from(resolve(__dirname, 'examples/good.tfvars'))
+  dottfvars.from(resolve(__dirname, 'examples/good.tfvars'))
   const imageId = env.from(process.env).get('image_id').asString()
   const availabilityZoneNames = env.from(process.env).get('availability_zone_names').asJsonArray()
   expect(typeof imageId).toBe('string')
@@ -11,7 +11,7 @@ test('parses .tfvars file', async () => {
 })
 
 test('parses tfvars.json file', async () => {
-  await dottfvars.from(resolve(__dirname, 'examples/good.tfvars.json'))
+  dottfvars.from(resolve(__dirname, 'examples/good.tfvars.json'))
   const imageId = env.from(process.env).get('image_id').asString()
   const availabilityZoneNames = env.from(process.env).get('availability_zone_names').asJsonArray()
   expect(typeof imageId).toBe('string')
